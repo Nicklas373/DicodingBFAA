7f6e1bd64418c4ab8d4289acded39ebcab3ceb0d
Dicky Herlambang <dicky@elwilis.com>
2021-10-11 11:18:15 +0700

    GithubUserProfileActivity: Use ViewModel and Mutable Live Data
    Signed-off-by: Dicky Herlambang <dicky@elwilis.com>

--- app/src/main/java/com/dicoding/dummyusersearch/activity/GithubUserProfileActivity.kt
+++ app/src/main/java/com/dicoding/dummyusersearch/activity/GithubUserProfileActivity.kt
@@ -3,24 +3,21 @@ package com.dicoding.dummyusersearch.activity
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.dicoding.dummyusersearch.R
-import com.dicoding.dummyusersearch.api.ApiConfig
 import com.dicoding.dummyusersearch.databinding.ActivityGithubUserProfileBinding
 import com.dicoding.dummyusersearch.userdata.GitHubUserJSON
+import com.dicoding.dummyusersearch.viewmodel.GitHubUserProfileActivityViewModel
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.squareup.picasso.Picasso
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class GithubUserProfileActivity : AppCompatActivity() {
 
@@ -33,17 +30,32 @@ class GithubUserProfileActivity : AppCompatActivity() {
         binding = ActivityGithubUserProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val userProfileViewModel =
+            ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
+                GitHubUserProfileActivityViewModel::class.java
+            )
+
         val sharedPref = this.getSharedPreferences(prefsName, Context.MODE_PRIVATE)
         val gitUser = sharedPref.getString(keyId, "null")
         val sectionsPagerAdapter = SectionPagerActivity(this)
         val viewPager: ViewPager2 = binding.viewPager
         val tabs: TabLayout = binding.tabs
 
+        userProfileViewModel.githubUserProfileJSON.observe(this, { userJSON ->
+            if (gitUser != null) {
+                setGitHubUserData(userJSON)
+            }
+        })
+
+        userProfileViewModel.isToast.observe(this, { isToast ->
+            showToast(isToast, userProfileViewModel.toastReason.value.toString())
+        })
+
         if (gitUser != null) {
-            getGitHubUserData(gitUser)
+            userProfileViewModel.getGitHubUserData(gitUser)
             title = gitUser
         } else {
-            getGitHubUserData("Null")
+            userProfileViewModel.getGitHubUserData("Null")
         }
 
         initTheme()
@@ -97,77 +109,58 @@ class GithubUserProfileActivity : AppCompatActivity() {
         }
     }
 
-    private fun getGitHubUserData(query: String) {
-        val client = ApiConfig.getApiService().getUserDetail(query)
-        client.enqueue(object : Callback<GitHubUserJSON> {
-            override fun onResponse(
-                call: Call<GitHubUserJSON>,
-                response: Response<GitHubUserJSON>
-            ) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        val gitNameText = binding.gitName
-                        val gitEmailText = binding.gitMail
-                        val gitLocationText = binding.gitLocation
-                        val gitImageDraw = binding.gitProfileImageview
-                        val gitCompanyText = binding.gitCompany
-                        val gitJoinText = binding.gitJoin
-                        val gitFollowersText = binding.gitFollowersCount
-                        val gitFollowingText = binding.gitFollowingCount
-                        val gitRepositoryText = binding.gitRepositoryCount
-                        val dateJoin: String? = responseBody.createdAt
-                        val splitDate = dateJoin?.substring(0, dateJoin.length - 10)
+    private fun setGitHubUserData(userJSON: GitHubUserJSON) {
+        val gitNameText = binding.gitName
+        val gitEmailText = binding.gitMail
+        val gitLocationText = binding.gitLocation
+        val gitImageDraw = binding.gitProfileImageview
+        val gitCompanyText = binding.gitCompany
+        val gitJoinText = binding.gitJoin
+        val gitFollowersText = binding.gitFollowersCount
+        val gitFollowingText = binding.gitFollowingCount
+        val gitRepositoryText = binding.gitRepositoryCount
 
-                        if (responseBody.name.isNullOrBlank()) {
-                            gitNameText.text = "-"
-                        } else {
-                            gitNameText.text = responseBody.name
-                        }
+        if (userJSON.name.isNullOrBlank()) {
+            gitNameText.text = "-"
+        } else {
+            gitNameText.text = userJSON.name
+        }
 
-                        if (responseBody.email.isNullOrBlank()) {
-                            gitEmailText.text = "-"
-                        } else {
-                            gitEmailText.text = responseBody.email
-                        }
+        if (userJSON.email.isNullOrBlank()) {
+            gitEmailText.text = "-"
+        } else {
+            gitEmailText.text = userJSON.email
+        }
 
-                        if (responseBody.location.isNullOrBlank()) {
-                            gitLocationText.text = "-"
-                        } else {
-                            gitLocationText.text = responseBody.location
-                        }
+        if (userJSON.location.isNullOrBlank()) {
+            gitLocationText.text = "-"
+        } else {
+            gitLocationText.text = userJSON.location
+        }
 
-                        if (responseBody.company.isNullOrBlank()) {
-                            gitCompanyText.text = "-"
-                        } else {
-                            gitCompanyText.text = responseBody.company
-                        }
+        if (userJSON.company.isNullOrBlank()) {
+            gitCompanyText.text = "-"
+        } else {
+            gitCompanyText.text = userJSON.company
+        }
 
-                        gitJoinText.text = splitDate
-                        gitFollowersText.text = responseBody.followers
-                        gitFollowingText.text = responseBody.following
-                        gitRepositoryText.text = responseBody.publicRepos
-                        Picasso.get().load(responseBody.avatarUrl).into(gitImageDraw)
-                    }
-                } else {
-                    Toast.makeText(
-                        this@GithubUserProfileActivity,
-                        "onFailure: ${response.message()}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
+        if (userJSON.createdAt.isNullOrBlank()) {
+            gitJoinText.text = "-"
+        } else {
+            val splitDate = userJSON.createdAt.substring(0, userJSON.createdAt.length - 10)
+            gitJoinText.text = splitDate
+        }
 
-            override fun onFailure(call: Call<GitHubUserJSON>, t: Throwable) {
-                Toast.makeText(
-                    this@GithubUserProfileActivity,
-                    "onFailure: ${t.message}",
-                    Toast.LENGTH_SHORT
-                ).show()
-                Log.e(TAG, "onFailure: ${t.message}")
-            }
-        })
+        gitFollowersText.text = userJSON.followers
+        gitFollowingText.text = userJSON.following
+        gitRepositoryText.text = userJSON.publicRepos
+        Picasso.get().load(userJSON.avatarUrl).into(gitImageDraw)
+    }
+
+    private fun showToast(isToast: Boolean, toastReason: String) {
+        if (!isToast) {
+            Toast.makeText(this, toastReason, Toast.LENGTH_LONG).show()
+        }
     }
 
     companion object {
@@ -177,7 +170,6 @@ class GithubUserProfileActivity : AppCompatActivity() {
             R.string.tab_text_2
         )
 
-        private val TAG = GithubUserProfileActivity::class.java.simpleName
         private const val prefsName = "TEMP_ID"
         private const val keyId = "key_id"
         private const val themeId = "theme_id"
---
+++ app/src/main/java/com/dicoding/dummyusersearch/viewmodel/GitHubUserProfileActivityViewModel.kt
@@ -0,0 +1,54 @@
+package com.dicoding.dummyusersearch.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.dicoding.dummyusersearch.api.ApiConfig
+import com.dicoding.dummyusersearch.userdata.GitHubUserJSON
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class GitHubUserProfileActivityViewModel : ViewModel() {
+
+    private val _githubUserProfileJSON = MutableLiveData<GitHubUserJSON>()
+    val githubUserProfileJSON: LiveData<GitHubUserJSON> = _githubUserProfileJSON
+
+    private val _isToast = MutableLiveData<Boolean>()
+    val isToast: LiveData<Boolean> = _isToast
+
+    private val _toastReason = MutableLiveData<String>()
+    val toastReason: LiveData<String> = _toastReason
+
+    fun getGitHubUserData(query: String) {
+        val client = ApiConfig.getApiService().getUserDetail(query)
+        client.enqueue(object : Callback<GitHubUserJSON> {
+            override fun onResponse(
+                call: Call<GitHubUserJSON>,
+                response: Response<GitHubUserJSON>
+            ) {
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        _githubUserProfileJSON.value = responseBody
+                    }
+                } else {
+                    _isToast.value = false
+                    _toastReason.value = "onFailure: ${response.message()}"
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<GitHubUserJSON>, t: Throwable) {
+                _isToast.value = false
+                _toastReason.value = "onFailure: ${t.message}"
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+
+    companion object {
+        private val TAG = GitHubUserProfileActivityViewModel::class.java.simpleName
+    }
+}
